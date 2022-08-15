cc9612385156abc0b72fe4b448722b2d9ff0ce79
navitacion <masataka.i.navi@gmail.com>
2022-08-15 23:35:10 +0900

    refactoring

---
+++ ensemble.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+import pandas as pd
+
+paths = [str(p) for p in Path("./ensembles").glob("**/*.csv")]
+
+print(paths)
+
+weights = [0.4, 0.4, 0.2]
+
+sub = pd.read_csv("./input/sample_submission.csv")
+sub = sub.sort_values(by="customer_ID").reset_index(drop=True)
+sub["prediction"] = 0
+
+for path, w in zip(paths, weights):
+    _df = pd.read_csv(path)
+    _df = _df.sort_values(by="customer_ID").reset_index(drop=True)
+
+    sub["prediction"] += _df["prediction"] * w
+
+# sub["prediction"] /= len(paths)
+
+sub.to_csv("ensemble.csv", index=False)
